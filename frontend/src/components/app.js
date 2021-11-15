07af013514511130be2d2f6bd3a266c949f50b67
cheemaz1 <zayedcheema@hotmail.com>
2021-11-15 18:22:11 -0500

    added deadlines backend

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,14 +4,14 @@ import { Switch } from 'react-router-dom';
 // import NavBarContainer from './nav/navbar_container';
 // import ProfileContainer from './profile/profile_container';
 import TasksContainer from './task/tasks_container';
-// import MainPage from './main/main_page';
+import MainPage from './main/main_page';
 import TaskComposeContainer from './task/create_task_container';
 
 const App = () => (
     <div>
     
         <Switch>
-            {/* <AuthRoute exact path="/" component={MainPage} /> */}
+            <AuthRoute exact path="/" component={MainPage} />
             <ProtectedRoute exact path="/tasks" component={TasksContainer} />
             {/* <ProtectedRoute exact path="/profile" component={ProfileContainer} /> */}
             <ProtectedRoute exact path="/new_task" component={TaskComposeContainer} />
