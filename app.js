fda466866718377048041666b4081a2166bffec1
AdeshPawaroo <adeshpawaroo1@gmail.com>
2021-11-15 10:56:21 -0500

    finish basic flashcard route

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
-// const express = require("express");
-// const app = express();
-// const mongoose = require('mongoose');
-// const db = require('./config/keys').mongoURI;
-// const users = require("./routes/api/users");
-// const User = require("./models/User");
-
-// mongoose
-//   .connect(db, { useNewUrlParser: true })
-//   .then(() => console.log("Connected to MongoDB successfully"))
-//   .catch(err => console.log(err));
-
-//   app.get("/", (req, res) => {
-//     const user = new User({
-//         handle: "jim",
-//         email: "jim@jim.jim",
-//         password: "jimisgreat123"
-//     });
-//     user.save();
-//     res.send("Hello World!");
-// });
-
-// app.use("/api/users", users)
-
-// const port = process.env.PORT || 5000;
-
-// app.listen(port, () => console.log(`Server is running on port ${port}`));
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
-const users = require("./routes/api/users");
-const User = require("./models/User");
 const bodyParser = require("body-parser");
-const passport = require('passport');
+const users = require("./routes/api/users")
+const User = require("./models/User");
+const flashcards = require("./routes/api/flashcards");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -57,6 +29,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", users)
+app.use("/api/flashcards", flashcards)
 
 const port = process.env.PORT || 5000;
 
