c1ba0f4aa3d8033e593d8dac4aa225a764bbbf1d
AdeshPawaroo <adeshpawaroo1@gmail.com>
2021-11-16 10:04:30 -0500

    fixed fetchUserFlashcards

--- frontend/src/components/flashcards/profile.jsx
+++ frontend/src/components/flashcards/profile.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 class Profile extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            flashcards: []
+        }
     }
 
     componentDidMount() {
         console.log(this.props.currentUser.id);
         this.props.fetchUserFlashcards(this.props.currentUser.id);
+    }
+
+    componentWillReceiveProps(newState) {
+        this.setState({ flashcards: newState.flashcards })
     }
 
     render() {
