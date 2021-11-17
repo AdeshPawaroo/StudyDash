5df3e5db7a819d6db5f263c5abf1d0d97ddb7876
efren <efrenbahen@gmail.com>
2021-11-17 17:28:52 -0600

    add event form

--- frontend/src/components/calendar/calendar.js
+++ frontend/src/components/calendar/calendar.js
@@ -6,6 +6,7 @@ import startOfWeek from 'date-fns/startOfWeek';
 import getDay from 'date-fns/getDay';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
 
 export default function CalenderContainer() {
@@ -139,12 +140,33 @@ export default function CalenderContainer() {
         }
     ]
 
+    const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""});
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent(){
+        setAllEvents([...allEvents, newEvent])
+    }
+
     return (
         <div className="calender-container">
+
             <Calendar localizer={localizer} events={allEvents} 
             startAccessor="start" endAccessor="end" 
-            style={{height: 500, margin: "50px"}}/>
+            style={{height: 600, margin: "50px"}} />
+
+            <div>
+                <input type="text" placeholder="Add title" style={{width: "20%", marginRight: "10px"}}
+                value={newEvent.title} onChange={(e) => setNewEvent({...newEvent, title: e.target.value})} />
+
+                <DatePicker placeholderText="Start date" style={{marginRight: "10px"}} 
+                selected={newEvent.start} onChange={(start) => setNewEvent({...newEvent, start})} />
             
+                <DatePicker placeholderText="End date" selected={newEvent.end} 
+                onChange={(end) => setNewEvent({...newEvent, end})} />
+
+                <button style={{marginTop: "10px"}} onClick={() => handleAddEvent}>Add event</button>
+            </div>
+
         </div>
     )
 }
