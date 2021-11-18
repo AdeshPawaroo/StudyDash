0e9e830296d66bd67224f6c98d151315e428013f
cheemaz1 <zayedcheema@hotmail.com>
2021-11-18 00:32:10 -0500

    heroku ready

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const Todo = require('./../../models/todo');
+const Todo = require('./../../models/Todo');
 const router = express.Router();
 
 // METHOD  : GET
